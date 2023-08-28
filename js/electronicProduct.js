6f1628673b65f39100679337f3bf9ef1b3167493
Sahil <sahilismailnavar@gmail.com>
2023-08-28 17:28:12 +0530

    Added producs of top deals on electronics

---
+++ js/electronicProduct.js
@@ -0,0 +1,61 @@
+// ************ top deals on electronics **************
+
+export const electronicProductData = [
+    {
+        img: "https://rukminim2.flixcart.com/image/200/200/xif0q/headphone/0/v/m/nord-buds-2-oneplus-original-imagnwf7sgjetqwg.jpeg?q=70",
+        productName: "Treuwireless Earphones",
+        discount: "From ₹499",
+        brand: "Apple, boAt, OnePlus & More",
+        link: "#"
+    },
+    {
+        img: "https://rukminim2.flixcart.com/image/200/200/xif0q/smartwatch/o/l/i/-original-imagpsqjgdfzcez7.jpeg?q=70",
+        productName: "Fire-Boltt Smartwatches",
+        discount: "From ₹1,099",
+        brand: "Best Deals For Limited Period!",
+        link: "#"
+    },
+    {
+        img: "https://rukminim2.flixcart.com/image/200/200/kr6oeq80/trimmer/a/e/m/0-5-15-mm-ng-1152-05-usb-stainless-steel-cordless-nova-original-imag5fybfhkdguru.jpeg?q=70",
+        productName: "Best of Trimmers",
+        discount: "From ₹399",
+        brand: "Philips, Mi & more",
+        link: "#"
+    },
+    {
+        img: "https://rukminim2.flixcart.com/image/200/200/kgfg2vk0/inktoner/7/x/s/epson-c13t00v198-original-imafwnbvxdrkv2vy.jpeg?q=70",
+        productName: "Printer Inks and Toners",
+        discount: "Up to 10% Off",
+        brand: "Canon, HP, Epson & more",
+        link: "#"
+    },
+    {
+        img: "https://rukminim2.flixcart.com/image/200/200/kb5eikw0/tablet/y/g/t/samsung-sm-p615nziainu-original-imafskyntyphkpbh.jpeg?q=70",
+        productName: "Samsung S6 Lite",
+        discount: "From ₹22,999*",
+        brand: "Get ₹3,000 Off for Students",
+        link: "#"
+    },
+    {
+        img: "https://rukminim2.flixcart.com/image/200/200/l31x2fk0/router/w/k/6/mr80x-mercusys-original-image9gvp3aefm2a.jpeg?q=70",
+        productName: "Top Deals On Routers!",
+        discount: "From ₹799",
+        brand: "Shop Now",
+        link: "#"
+    },
+    {
+        img: "https://rukminim2.flixcart.com/image/200/200/xif0q/shopsy-power-bank/o/x/o/pp-111-10000-ambrane-original-imagg7nqgb8zr3zt.jpeg?q=70",
+        productName: "Ambrane Powerbanks",
+        discount: "From ₹799",
+        brand: "Fast Charging | High Capacity",
+        link: "#"
+    },
+    {
+        img: "https://rukminim2.flixcart.com/fk-p-flap/200/200/image/d508237c2df9fd4a.png?q=70",
+        productName: "Screen Guards",
+        discount: "From ₹99",
+        brand: "For All Top Models",
+        link: "#"
+    },
+
+]
