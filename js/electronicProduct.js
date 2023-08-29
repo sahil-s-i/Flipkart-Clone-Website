e2fefcce31f6dcdaeaaa14e5cc9051a1a8ad793f
Sahil <sahilismailnavar@gmail.com>
2023-08-29 16:23:41 +0530

    Added beauty and toys part

--- js/electronicProduct.js
+++ js/electronicProduct.js
@@ -59,3 +59,63 @@ export const electronicProductData = [
     },
 
 ]
+
+// ************ beauty food toys and more  **************
+export const BeautyfoodandmoreProductData = [
+    {
+        img: "https://rukminim2.flixcart.com/image/200/200/k6fd47k0/nut-dry-fruit/p/z/7/200-100-natural-california-pouch-happilo-original-imafzvw2tcazeur6.jpeg?q=70",
+        productName: "Dry Fruits",
+        discount: "Upto 75% Off",
+        brand: "Happilo, Farmley & More",
+        link: ""
+    },
+    {
+        img: "https://rukminim2.flixcart.com/image/200/200/kkimfm80/tea/z/n/1/premium-pouch-regular-tea-powder-tata-original-imafzuf2mnubzphd.jpeg?q=70",
+        productName: "Tea Powder",
+        discount: "Upto 75% Off",
+        brand: "Tata,Lipton & More",
+        link: ""
+    },
+    {
+        img: "https://rukminim2.flixcart.com/image/200/200/klzhq4w0/jam-spread/w/q/u/510-chocolate-peanut-butter-crunchy-510g-jar-nut-butter-original-imagyzpqqfaguxny.jpeg?q=70",
+        productName: "Food Spreads",
+        discount: "Upto 75% Off",
+        brand: "Myfiness,Alpino & More",
+        link: ""
+    },
+    {
+        img: "https://rukminim2.flixcart.com/image/200/200/l111lzk0/cycle/m/r/e/xc-900-grey-lite-26-15-5-cradiac-21-gear-120-original-imagczuzpxeweczm.jpeg?q=70",
+        productName: "Geared Cycles",
+        discount: "From ₹ 3999",
+        brand: "Leader, Cradiac & more",
+        link: ""
+    },
+    {
+        img: "https://rukminim2.flixcart.com/image/200/200/kz5vwy80/helmet/o/4/k/-original-imagb8azfdthjhqr.jpeg?q=70",
+        productName: "Rider Helmets",
+        discount: "From 699",
+        brand: "Vega, Steelbird, HRX and More",
+        link: ""
+    },
+    {
+        img: "https://rukminim2.flixcart.com/image/200/200/jxz0brk0/stuffed-toy/n/t/s/4-feet-pink-very-beautiful-best-quality-for-special-gift-125-13-original-imafgv92puzkdytg.jpeg?q=70",
+        productName: "Soft Toys",
+        discount: "Upto 70% Off",
+        brand: "Stuffed Toys, Plush Toys & more",
+        link: ""
+    },
+    {
+        img: "https://rukminim2.flixcart.com/image/200/200/l58iaa80/electric-cycle/i/y/f/-original-imagfykthgudy4qz.jpeg?q=70",
+        productName: "Electric Cycle",
+        discount: "Up to 40% Off",
+        brand: "Nuze, Motovolt & more",
+        link: ""
+    },
+    {
+        img: "https://rukminim2.flixcart.com/image/200/200/kdj4xow0/treadmill/b/g/c/ft098-steel-motorized-ft98-steel-motorized-motorized-fitkit-original-imafuerskytashcz.jpeg?q=70",
+        productName: "Treadmill, Exercise Bikes & More",
+        discount: "Up to 70% Off",
+        brand: "Fitkit, Powermax & More",
+        link: ""
+    },
+]
--- js/poduct.js
+++ js/poduct.js
@@ -1,4 +1,5 @@
 import { electronicProductData } from "./electronicProduct.js";
+import { BeautyfoodandmoreProductData } from "./electronicProduct.js";
 
 
 // ************ top deals on electronics **************
@@ -24,3 +25,27 @@ electronicProductData.forEach(el => {
         `
 });
 bestofElectronic_Product_itemEl.innerHTML = bestofElectronicProduct_html
+
+// ************ beauty food toys and more  **************
+
+let bestofElectronic_Product_itemE2 = document.querySelector(".bestofbeautyfoodtoys_Product_item")
+let bestofbeautyfoodtoys_Product_item = ""
+
+BeautyfoodandmoreProductData.forEach(el => {
+    bestofbeautyfoodtoys_Product_item += `
+        <a href="${el.link}" >
+            <div class="bestofElectronic_item">
+                <div class="bestofElectronic_image_product">
+                    <img src="${el.img}"
+                        alt="">
+                </div>
+                <div class="bestofElectronicmoreOption">
+                    <p class="bestofElectronicProduct_name">${el.productName}</p>
+                    <p class="bestofElectronic_discount">${el.discount}</p>
+                    <p class="bestofElectronic_brand">${el.brand}</p>
+                </div>
+            </div>
+        </a>
+        `
+});
+bestofElectronic_Product_itemE2.innerHTML = bestofbeautyfoodtoys_Product_item
